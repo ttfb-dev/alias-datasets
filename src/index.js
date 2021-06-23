0617217cb652e526517950934441856da5f800ec
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-06-23 21:44:14 +0300

    add datasets to response

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ app.post('/user/:user_id/activate-dataset', async (req, res) => {
     const userId = req.params.user_id;
     const datasetId = req.body.datasetId
     try {
-        await userDatasetHandler.activate(userId, datasetId);
-        res.status(200).send();
+        const datasets = await userDatasetHandler.activate(userId, datasetId);
+        res.status(200).json(datasets);
     } catch (e) {
         logger.critical('Activate dataset failed', {method: '/user/activate-dataset', userId, datasetId})
         res.status(400).send();
@@ -23,8 +23,8 @@ app.post('/user/:user_id/deactivate-dataset', async (req, res) => {
     const userId = req.params.user_id;
     const datasetId = req.body.datasetId;
     try {
-        await userDatasetHandler.deactivate(userId, datasetId);
-        res.status(200).send();
+        const datasets = await userDatasetHandler.deactivate(userId, datasetId);
+        res.status(200).json(datasets);
     } catch (e) {
         logger.critical('Deactivate dataset failed', {method: '/user/deactivate-dataset', userId, datasetId})
         res.status(400).send();
@@ -34,8 +34,8 @@ app.post('/user/:user_id/deactivate-dataset', async (req, res) => {
 app.post('/user/:user_id/active', async (req, res) => {
     const userId = req.params.user_id;
     try {
-        const activeDatasets = await userDatasetHandler.getActive(userId, datasetId);
-        res.status(200).json(activeDatasets);
+        const datasets = await userDatasetHandler.getActive(userId, datasetId);
+        res.status(200).json(datasets);
     } catch (e) {
         logger.critical('Get activate dataset failed', {method: '/user/activate', userId})
         res.status(400).send();
