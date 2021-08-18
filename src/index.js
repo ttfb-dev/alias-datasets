6f801300c23811b5225f0ef95e5ff445ebc1d3de
Dmitry Bobrov <d.bobrov@corp.vk.com>
2021-08-18 22:37:06 +0300

    format

--- src/index.js
+++ src/index.js
@@ -1,150 +1,164 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import userDatasetHandler from './userDatasetHandler.js'
-import gameDatasetHandler from './gameDatasetHandler.js'
-import datasetHandler from './datasetHandler.js'
-import logger from './logger.js'
+import express from "express";
+import bodyParser from "body-parser";
+import userDatasetHandler from "./userDatasetHandler.js";
+import gameDatasetHandler from "./gameDatasetHandler.js";
+import datasetHandler from "./datasetHandler.js";
+import logger from "./logger.js";
 
-const app = express()
-const port = 80
-app.use(bodyParser.json())
+const app = express();
+const port = 80;
+app.use(bodyParser.json());
 
-app.get('/room/:room_id/active', async (req, res) => {
+app.get("/room/:room_id/active", async (req, res) => {
   const roomId = parseInt(req.params.room_id);
   try {
     const datasets = await gameDatasetHandler.getActive(roomId);
     res.status(200).json(datasets);
   } catch (e) {
-      logger.critical(e.message, {method: `/room/${roomId}/active`})
-      res.status(400).send();
+    logger.critical(e.message, { method: `/room/${roomId}/active` });
+    res.status(400).send();
   }
-})
+});
 
-app.post('/room/:room_id/activate-dataset', async (req, res) => {
+app.post("/room/:room_id/activate-dataset", async (req, res) => {
   const roomId = parseInt(req.params.room_id);
   const datasetId = req.body.datasetId;
   try {
     const datasets = await gameDatasetHandler.activate(roomId, datasetId);
     res.status(200).json(datasets);
   } catch (e) {
-      logger.critical(e.message, {method: `/room/${roomId}/activate-dataset`})
-      res.status(400).send();
+    logger.critical(e.message, { method: `/room/${roomId}/activate-dataset` });
+    res.status(400).send();
   }
-})
+});
 
-app.post('/room/:room_id/deactivate-dataset', async (req, res) => {
+app.post("/room/:room_id/deactivate-dataset", async (req, res) => {
   const roomId = parseInt(req.params.room_id);
   const datasetId = req.body.datasetId;
   try {
     const datasets = await gameDatasetHandler.deactivate(roomId, datasetId);
     res.status(200).json(datasets);
   } catch (e) {
-      logger.critical(e.message, {method: `/room/${roomId}/deactivate-dataset`})
-      res.status(400).send();
+    logger.critical(e.message, {
+      method: `/room/${roomId}/deactivate-dataset`,
+    });
+    res.status(400).send();
   }
-})
+});
 
-app.get('/datasets', async (req, res) => {
+app.get("/datasets", async (req, res) => {
   try {
     const datasets = await datasetHandler.getList();
     res.status(200).json(datasets);
   } catch (e) {
-      logger.critical(e.message, {method: '/datasets'})
-      res.status(400).send();
+    logger.critical(e.message, { method: "/datasets" });
+    res.status(400).send();
   }
-})
+});
 
-app.get('/datasets/type/game', async (req, res) => {
+app.get("/datasets/type/game", async (req, res) => {
   try {
     const datasets = await datasetHandler.getGameList();
     res.status(200).json(datasets);
   } catch (e) {
-      logger.critical(e.message, {method: '/datasets/type/game'})
-      res.status(400).send();
+    logger.critical(e.message, { method: "/datasets/type/game" });
+    res.status(400).send();
   }
-})
+});
 
-app.get('/datasets/:dataset_id', async (req, res) => {
+app.get("/datasets/:dataset_id", async (req, res) => {
   const datasetId = parseInt(req.params.dataset_id);
   try {
     const dataset = await datasetHandler.getById(datasetId);
     if (dataset) {
       res.status(200).json(dataset);
-      return ;
+      return;
     }
-    logger.critical('Call to unknown dataset', {method: `/datasets/${datasetId}`})
+    logger.critical("Call to unknown dataset", {
+      method: `/datasets/${datasetId}`,
+    });
     res.status(404).send();
   } catch (e) {
-      logger.critical(e.message, {method: `/datasets/${datasetId}`})
-      res.status(400).send();
+    logger.critical(e.message, { method: `/datasets/${datasetId}` });
+    res.status(400).send();
   }
-})
+});
 
-app.get('/datasets/:dataset_id/words', async (req, res) => {
+app.get("/datasets/:dataset_id/words", async (req, res) => {
   const datasetId = parseInt(req.params.dataset_id);
   try {
     const words = await datasetHandler.getWords(datasetId);
     if (words) {
       res.status(200).json(words);
-      return ;
+      return;
     }
-    logger.critical('Words not found', {method: `/datasets/${datasetId}/words`})
+    logger.critical("Words not found", {
+      method: `/datasets/${datasetId}/words`,
+    });
     res.status(404).send();
   } catch (e) {
-      logger.critical(e.message, {method: `/datasets/${datasetId}/words`})
-      res.status(400).send();
+    logger.critical(e.message, { method: `/datasets/${datasetId}/words` });
+    res.status(400).send();
   }
-})
+});
 
-app.post('/datasets/:dataset_id/words', async (req, res) => {
+app.post("/datasets/:dataset_id/words", async (req, res) => {
   const datasetId = parseInt(req.params.dataset_id);
   const words = req.body.words;
   try {
     await datasetHandler.setWords(datasetId, words);
     res.status(200).send();
   } catch (e) {
-      logger.critical(e.message, {method: `/datasets/${datasetId}/words`})
-      res.status(400).send();
+    logger.critical(e.message, { method: `/datasets/${datasetId}/words` });
+    res.status(400).send();
   }
-})
+});
 
-app.post('/user/:user_id/activate-dataset', async (req, res) => {
-    const userId = parseInt(req.params.user_id);
-    const datasetId = req.body.datasetId
-    try {
-        const datasets = await userDatasetHandler.activate(userId, datasetId);
-        res.status(200).json(datasets);
-    } catch (e) {
-        logger.critical(e.message, {method: `/user/${user}/activate-dataset`, userId, datasetId})
-        res.status(400).send();
-    }
-})
+app.post("/user/:user_id/activate-dataset", async (req, res) => {
+  const userId = parseInt(req.params.user_id);
+  const datasetId = req.body.datasetId;
+  try {
+    const datasets = await userDatasetHandler.activate(userId, datasetId);
+    res.status(200).json(datasets);
+  } catch (e) {
+    logger.critical(e.message, {
+      method: `/user/${user}/activate-dataset`,
+      userId,
+      datasetId,
+    });
+    res.status(400).send();
+  }
+});
 
-app.post('/user/:user_id/deactivate-dataset', async (req, res) => {
-    const userId = parseInt(req.params.user_id);
-    const datasetId = req.body.datasetId;
-    try {
-        const datasets = await userDatasetHandler.deactivate(userId, datasetId);
-        res.status(200).json(datasets);
-    } catch (e) {
-        logger.critical(e.message, {method: `/user/${user}/deactivate-dataset`, userId, datasetId})
-        res.status(400).send();
-    }
-})
+app.post("/user/:user_id/deactivate-dataset", async (req, res) => {
+  const userId = parseInt(req.params.user_id);
+  const datasetId = req.body.datasetId;
+  try {
+    const datasets = await userDatasetHandler.deactivate(userId, datasetId);
+    res.status(200).json(datasets);
+  } catch (e) {
+    logger.critical(e.message, {
+      method: `/user/${user}/deactivate-dataset`,
+      userId,
+      datasetId,
+    });
+    res.status(400).send();
+  }
+});
 
-app.get('/user/:user_id/active', async (req, res) => {
-    const userId = parseInt(req.params.user_id);
-    try {
-        const datasets = await userDatasetHandler.getActive(userId);
-        res.status(200).json(datasets);
-    } catch (e) {
-        logger.critical(e.message, {method: `/user/${user}/activate`, userId})
-        res.status(400).send();
-    }
-})
+app.get("/user/:user_id/active", async (req, res) => {
+  const userId = parseInt(req.params.user_id);
+  try {
+    const datasets = await userDatasetHandler.getActive(userId);
+    res.status(200).json(datasets);
+  } catch (e) {
+    logger.critical(e.message, { method: `/user/${user}/activate`, userId });
+    res.status(400).send();
+  }
+});
 
-app.get('/', async (req, res) => {
-    res.status(200).send();
-})
+app.get("/", async (req, res) => {
+  res.status(200).send();
+});
 
-app.listen(port)
+app.listen(port);
